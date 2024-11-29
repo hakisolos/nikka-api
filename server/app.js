3505fc620a6355467b5640a6535f253aadd18964
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-11-29 23:22:39 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -84,6 +84,9 @@ app.get("/andiff", limit, async (req, res) => {
 app.get("/yts", limit, async (req, res) => {
     require("../pages/fitures/ytsearch.js")(req, res)
 })
+app.get("/ssweb", limit, async (req, res) => {
+    require("../pages/fitures/ssweb.js")(req, res)
+})
 app.get("/instagramDL", limit, async (req, res) => {
     require("../pages/fitures/instagram.js")(req, res)
 })
