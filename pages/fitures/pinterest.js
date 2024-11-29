d449d603ddf340da1cf88ebf35d9f4555f30ebda
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-11-29 13:11:05 +0100

    Update pinterest.js

--- pages/fitures/pinterest.js
+++ pages/fitures/pinterest.js
@@ -1,1 +1,6 @@
+const axios = require("axios");
+const allowedApiKeys = require("../../declaration/arrayKey.jsx");
 
+module.exports = async (req, res) => {
+  const url = req.query.url; // Changed from `urls` to `url`
+  const apiKey = req.query.apiKey;
