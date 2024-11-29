d05294b13f65aeb2d466b4bdfb375be5e5f233e3
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-11-29 11:46:54 -0800

    Update checksite.js

--- pages/fitures/checksite.js
+++ pages/fitures/checksite.js
@@ -116,3 +116,43 @@ module.exports = async (req, res) => {
   }
 }
 */
+
+const axios = require("axios")
+const allowedApiKeys = require("../../declaration/arrayKey.jsx")
+
+module.exports = async (req, res) => {
+
+  const message = req.query.message
+  const apiKey = req.query.apiKey
+
+  if (!q) {
+    return res.status(400).json({
+      error: "provide url"
+    })
+  }
+
+  if (!apiKey) {
+    return res.status(403).json({
+      error: "Input Parameter Apikey!"
+    })
+  } else if (!allowedApiKeys.includes(apiKey)) {
+    return res.status(403).json({
+      error: "apikey not found"
+    })
+  }
+
+  const url = `https://api.agatz.xyz/api/google?message=${message}`
+
+  try {
+    const response = await axios.get(url)
+    const data = response.data.summary
+    res.status(200).json({
+      data
+    })
+  } catch (error) {
+    res.status(500).json({
+      error: "err, fedk up"
+    })
+  }
+}
+
