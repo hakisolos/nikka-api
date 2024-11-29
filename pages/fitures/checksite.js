e751c7490c7e7e6b64f65808da1db175f53a247e
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-11-29 11:36:00 -0800

    Update checksite.js

--- pages/fitures/checksite.js
+++ pages/fitures/checksite.js
@@ -2,7 +2,7 @@
    * by balzz
    * dont delete my wm
    * follow more instagram: @iqstore78
-*/
+
 
 const axios = require("axios");
 const allowedApiKeys = require("../../declaration/arrayKey.jsx"); // Adjust the path to your allowed API keys
@@ -72,3 +72,47 @@ module.exports = async (req, res) => {
     });
   }
 };
+*/
+/*
+   * by balzz
+   * dont delate my wm
+   * follow more instagram: @iqstore78
+*/
+const axios = require("axios")
+const allowedApiKeys = require("../../declaration/arrayKey.jsx")
+
+module.exports = async (req, res) => {
+
+  const q = req.query.url
+  const apiKey = req.query.apiKey
+
+  if (!q) {
+    return res.status(400).json({
+      error: "provide url"
+    })
+  }
+
+  if (!apiKey) {
+    return res.status(403).json({
+      error: "Input Parameter Apikey!"
+    })
+  } else if (!allowedApiKeys.includes(apiKey)) {
+    return res.status(403).json({
+      error: "apikey not found"
+    })
+  }
+
+  const url = `https://itzpire.com/tools/about-website?url=${q}`
+
+  try {
+    const response = await axios.get(url)
+    const data = response.data.result
+    res.status(200).json({
+      data
+    })
+  } catch (error) {
+    res.status(500).json({
+      error: "err, fedk up"
+    })
+  }
+}
