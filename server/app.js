2933544164a80e3ac4f73b2b82ec4cbd0b48fe90
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-01 14:15:17 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.use(session({
 
 /* !=== PAGE ===! */
 app.get("/", limit, (req, res) => {
-    res.sendFile(path.join(__dirname, "../pages/404.html"))
+    res.sendFile(path.join(__dirname, "../pages/docs/index.html"))
 })
 
 app.get("/login", limit, (req, res) => {
@@ -107,7 +107,7 @@ app.get("/instagramDL", limit, async (req, res) => {
 })
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "../pages/docs/index.html"))
+  res.status(404).sendFile(path.join(__dirname, "../pages/404.html"))
 })
 
 module.exports = app
