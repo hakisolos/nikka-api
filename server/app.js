8c29b58202dc863c90d60ed316d2ba74b630eb8d
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-01 14:12:46 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -107,7 +107,7 @@ app.get("/instagramDL", limit, async (req, res) => {
 })
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "../pages/404.html"))
+  res.status(404).sendFile(path.join(__dirname, "../pages/docs/index.html"))
 })
 
 module.exports = app
